c81b6784fb4307476662062300e166bcf950bec5
Kyle Ebersole <kyle.m.ebersole@gmail.com>
2017-09-15 14:51:25 -0700

    Add script for form submission. Update comments in script to clarify each section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function(event){   /* INITIAL DOC */
 
+/* Toggle between questions */
   $("button#buttonSubmit").click(function(){ /* SUBMIT BUTTON */
     $("#qOne").fadeToggle();
     $("button#buttonSubmit").hide();
-  }); /* SUBMIT BUTTON */
+  });
   $("button#buttonOne").click(function(){ /* Q1 */
     $("#qOne").slideToggle();
     $("#qTwo").slideToggle();
     $("button#buttonRestart").fadeIn();
-  }); /* Q1 */
+  });
   $("button#buttonRestart").click(function(){ /* RESTART BUTTON */
     $("#qOne").fadeIn();
     $("button#buttonSubmit").hide();
@@ -16,20 +17,25 @@ $(document).ready(function(event){   /* INITIAL DOC */
     $("#qThree").hide();
     $("#qFour").hide();
     $("#qFive").hide();
-  }); /* RESTART BUTTON */
+  });
   $("button#buttonTwo").click(function(){ /* Q2 */
     $("#qTwo").slideToggle();
     $("#qThree").slideToggle();
-  }); /* Q2 */
+  });
   $("button#buttonThree").click(function(){ /* Q3 */
     $("#qThree").slideToggle();
     $("#qFour").slideToggle();
-  }); /* Q3 */
+  });
   $("button#buttonFour").click(function(){ /* Q4 */
     $("#qFour").slideToggle();
     $("#qFive").slideToggle();
-  }); /* Q4 */
-  $("button#buttonFive").click(function(){ /* QFinal */
+  });
+
+/* Submit inputs & show result! */
+  $("form#survey").submit(function(event){ /* SURVEY FORM */
     alert("CONGRATS TESTING WORKED");
-  }); /* QFinal */
+
+  event.preventDefault();
+  }); /* SURVEY FORM */
+
 });  /* INITIAL DOC */
